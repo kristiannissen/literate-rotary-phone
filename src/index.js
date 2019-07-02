a60ded4b92574af76a183b4cc71de12719a13cb1
Kristian Nissen <kristian.nissen@gmail.com>
2019-07-02 20:28:38 +0200

    Hello Kitty works

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import AppComponent from './AppComponent.vue'
 
-new Vue({
+/*new Vue({
    render: h => h(AppComponent)
  }).$mount('#app')
+ */
+
+Vue.component('appcomponent', AppComponent)
+
+const app = new Vue({
+    el: '#app'
+})
