46df32c018c8835b33ca6af1f2e4561ede44c1b8
Kristian Nissen <kristian.nissen@gmail.com>
2019-07-05 13:58:57 +0200

    Skeleton

--- src/components/GroceryList.vue
+++ src/components/GroceryList.vue
@@ -1,115 +1,92 @@
 <template>
-  <div class="mdl-grid mdl-grid--no-spacing">
-    <div class="mdl-grid mdl-grid--no-spacing">
-      <div class="mdl-cell mdl-cell--12-col">
-        <h3>{{ name }}</h3>
-      </div>
-    </div>
-    <div class="mdl-grid">
-      <div class="mdl-grid mdl-grid--no-spacing">
-        <div class="mdl-cell mdl-cell--12-col"><h4>Yeasts</h4></div>
-      </div>
-      <div class="mdl-grid mdl-grid--no-spacing">
-        <div class="mdl-cell mdl-cell--12-col">
-          <ul class="mdl-list">
-            <li
-              class="mdl-list__item mdl-list__item--three-line"
-              v-for="item in yeastList"
-            >
-              <span class="mdl-list__item-primary-content">
-                <span>{{ item.name }}</span>
-                <span class="mdl-list__item-text-body">{{
-                  item.substitutes
-                }}</span>
-              </span>
-              <span class="mdl-list__item-secondary-content">
-                <label
-                  class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect"
-                  v-bind:for="item.id"
-                >
-                  <input
-                    type="checkbox"
-                    v-bind:id="item.id"
-                    v-bind:value="item.name"
-                    class="mdl-checkbox__input"
-                  />
-                </label>
-              </span>
+    <div class="grocery-list">
+        <ul class="mdl-list">
+            <li class="mdl-list__item">
+                <span class="mdl-list__item-primary-content">Yeast</span>
             </li>
-          </ul>
-        </div>
-      </div>
+            <li class="mdl-list__item mdl-list__item--three-line" v-for="item in yeastList">
+                <span class="mdl-list__item-primary-content">
+                    <span>{{ item.name }}</span>
+                    <span class="mdl-list__item-text-body">body</span>
+                </span>
+                <span class="mdl-list__item-secondary-action">
+                    <label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect" v-bind:for="item.id">
+                        <input type="checkbox" class="mdl-checkbox__input" v-bind:value="item.name" v-bind:id="item.id" />
+                    </label>
+                </span>
+            </li>
+        </ul>
     </div>
-  </div>
 </template>
 <script>
 export default {
   data() {
     return {
-      name: "Art Brew vol 3",
       yeasts: [
         {
-          name: "Imperial B63 Monastic"
+          name: "Imperial B63 Monastic",
+          substitutes: ""
         },
         {
-          name: "Imperial W15 Suburban Brett"
+          name: "Imperial W15 Suburban Brett",
+          substitutes: ""
         }
       ],
       fermentables: [
         {
           type: "grain",
           name: "Pilsner Malt",
-          amount: 5
+          amount: 5,
+          substitutes: ""
         },
         {
           type: "grain",
           name: "Vienna",
-          amount: 1
+          amount: 1,
+          substitutes: ""
         },
         {
           type: "grain",
           name: "Munich Malt",
-          amount: 0.5
+          amount: 0.5,
+          substitutes: ""
         },
         {
           type: "grain",
           name: "Cara Crystal",
-          amount: 0.5
+          amount: 0.5,
+          substitutes: ""
         },
         {
           type: "extract",
           name: "Dark Brown Sugar",
-          amount: 0.35
+          amount: 0.35,
+          substitutes: ""
         }
       ],
       hops: [
         {
           name: "Sorachi Ace",
-          amount: 0.023
+          amount: 0.023,
+          substitutes: ""
         },
         {
           name: "Amarillo",
-          amount: 0.05
+          amount: 0.05,
+          substitutes: ""
         }
       ]
     };
   },
-  mounted() {
-    console.log("Hello List");
-  },
-  created() {
-    console.log("Hello List");
-  },
+  mounted() {},
+  created() {},
   computed: {
     yeastList: function() {
       return this.yeasts.map(function(item) {
         return {
           name: item.name,
           id: item.name.toLowerCase().replace(/\W/g, "_"),
-          substitutes: [
-            "Wyeast 1214 Belgian Ale",
-            "White Labs 500 Monastery Ale"
-          ].join(", ")
+          substitutes: ""
         };
       });
     }
