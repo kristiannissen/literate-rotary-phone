b97ebefb3c377470f0ebf1b76c7315039677b67a
Kristian Nissen <kristian.nissen@gmail.com>
2019-07-08 12:00:27 +0200

    Action button added

--- src/components/ActionButton.vue
+++ src/components/ActionButton.vue
@@ -1,19 +1,42 @@
 <template>
-  <div style="position: absolute; bottom: 5px; right: 5px;">
-    <button
+  <div v-bind:class="['mdl-action-button', (active ? 'show-actions' : 'hide-actions')]">
+    <button v-on:click="toggleActions"
       class="mdl-button mdl-js-button mdl-button--fab mdl-button--colored"
     >
       <i class="material-icons">add</i>
     </button>
+    <ul>
+        <li>
+            <button class="mdl-button mdl-js-button">
+                <i class="material-icons">create</i>
+            </button>
+        </li>
+        <li>
+            <button class="mdl-button mdl-js-button">
+                <i class="material-icons">save</i>
+            </button>
+        </li>
+    </ul>
   </div>
 </template>
 <script>
 export default {
+  data() {
+    return {
+      active: false
+    };
+  },
   mounted() {
     console.log("Hello Kitty");
   },
   created() {
     console.log("Hello Kitty");
+  },
+  methods: {
+    toggleActions() {
+      if (this.active === false) this.active = true;
+      else this.active = false;
+    }
   }
 };
 </script>
--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
-import Vue from 'vue'
-import AppComponent from './AppComponent'
-import ActionButton from './components/ActionButton'
-import GroceryList from './components/GroceryList'
+import Vue from "vue";
+import AppComponent from "./AppComponent";
+import ActionButton from "./components/ActionButton";
+import GroceryList from "./components/GroceryList";
+import BrewTitle from "./components/BrewTitle";
 /*new Vue({
    render: h => h(AppComponent)
  }).$mount('#app')
  */
 
 // Vue.component('appcomponent', AppComponent)
-Vue.component('actionbutton', ActionButton)
-Vue.component('grocery-list', GroceryList)
+Vue.component("brew-title", BrewTitle);
+Vue.component("actionbutton", ActionButton);
+Vue.component("grocery-list", GroceryList);
 
 const app = new Vue({
-    el: '#app'
-})
+  el: "#app"
+});
